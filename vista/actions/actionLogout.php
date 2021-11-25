9790cb5246b661d16a44ea93886c7165555492b8
Santiago Scantamburlo <saflemsca@gmail.com>
2021-11-25 20:55:23 -0300

    Arreglo logout

--- vista/actions/actionLogout.php
+++ vista/actions/actionLogout.php
@@ -5,26 +5,3 @@ $sesion = new session();
 
 $sesion->cerrarSession();
 header('Location: ../home/index.php?message=' . urlencode("Sesión cerrada"));
-?>
-
-
-
-
-
-
-<?php
-echo "Ingrese la cantidad de numeros a comparar";
-$cantNums = trim(fgets(STDIN));
-echo "Ingrese un numero";
-$min = trim(fgets(STDIN)); //Asumo que el primer numero ingresado es el menor de la secuencia
-
-
-for ($i = 0; $i < $cantNums - 1; $i++) {
-    echo "Ingrese un numero";
-    $num = trim(fgets(STDIN));
-    if ($num <= $min & $num != $min) {
-        $min = $num;
-    }
-}
-
-echo "El menor numero ingresado es: $min";
